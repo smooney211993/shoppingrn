ffec269f623b6867a4695b35f4436ff0ca846194
stephen <sm.athome21@hotmail.com>
2020-12-05 00:10:05 +1100

    added product card

--- screens/ProductsOverview.tsx
+++ screens/ProductsOverview.tsx
@@ -3,6 +3,7 @@ import { FlatList, Text } from 'react-native';
 import { AppState } from '../store';
 import { useSelector } from 'react-redux';
 import { ShopNavProps } from '../navigation/ShopParamList';
+import ProductItem from '../components/shop/ProductItem';
 
 const ProductsOverview = ({ navigation }: ShopNavProps<'Product'>) => {
   const { availableProducts } = useSelector(
@@ -11,7 +12,15 @@ const ProductsOverview = ({ navigation }: ShopNavProps<'Product'>) => {
   return (
     <FlatList
       data={availableProducts}
-      renderItem={(item) => <Text>{item.item.title}</Text>}
+      renderItem={(item) => (
+        <ProductItem
+          imageUrl={item.item.imageUrl}
+          price={item.item.price}
+          id={item.item.id}
+          description={item.item.description}
+          title={item.item.title}
+        />
+      )}
     />
   );
 };
